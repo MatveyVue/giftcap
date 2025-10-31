96766d73e6c25e4ef0348f063348e76530585d9d
Whsxg <shabanov.matveyka@bk.ru>
2025-10-31 22:22:02 +0330

    Add files via upload

---
+++ scripts/transaction.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    if (typeof TON_CONNECT_UI === 'undefined') {
+        console.error(' Проверьте подключение к интернету');
+        alert('Error!');
+return;
+        }
+        const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+            manifestUrl: 'https://raw.githubusercontent.com/MatveyVue/caps/refs/heads/main/tonconnect-manifest.json'
+        });
+    
+        window.handlePayment = async function() {
+            try {
+                console.log('Инициализация платежа...');
+    
+            if (!tonConnectUI.connected) {
+                console.log('Подключение кошелька...');
+                await tonConnectUI.connectWallet();
+            }
+        const transaction = {
+            validUntil: Math.floor(Date.now() / 1000) + 360, 
+            messages: [
+                {
+                    address: 'UQDW5V6H3mx3p31qlkEHTkkewiXkqX8wRdOl5F6E0pWMXle4',
+                    amount: '500000000',
+                }
+            ]
+        };
+        console.log('Отправка транзакции...');
+        const result = await tonConnectUI.sendTransaction(transaction);
+        console.log('Транзакция успешно отправлена');
+        alert('The transaction successful');
+        } catch (error) {
+            console.error('Ошибка при оплате');
+            alert('The transaction failed');
+        }
+    };
+});
